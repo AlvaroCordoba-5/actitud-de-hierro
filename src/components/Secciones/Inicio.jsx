162f1e13ebcc67e7918cb5f02b56fb53496ce8f4
AlvaroCordoba-5 <cordobaalvaro16@gmail.com>
2022-11-17 11:26:14 -0300

    updates

--- src/components/Secciones/Inicio.jsx
+++ src/components/Secciones/Inicio.jsx
@@ -7,15 +7,6 @@ import Footer from "../Footer/Footer";
 import Whatsapp from "../Botones/Whatsapp";
 import s1 from '../imagenes/Servicios1.png'
 import s2 from '../imagenes/Servicios2.png'
-import f1 from '../imagenes/inicio1.jpg';
-import f2 from '../imagenes/inicio2.webp'
-import f3 from '../imagenes/inicio3.webp'
-import f4 from '../imagenes/inicio4.webp'
-import f5 from '../imagenes/inicio5.jpg'
-import f6 from '../imagenes/inicio6.jpg'
-import f7 from '../imagenes/inicio7.jpg'
-import f8 from '../imagenes/inicio8.jpg'
-import f9 from '../imagenes/inicio9.jpg'
 import "./Css/Inicio.css";
 
 function Inicio() {
@@ -81,18 +72,8 @@ function Inicio() {
 
       <GaleriaSlider />
 
-<GaleriaProductos
- img1={f1}
- img2={f2}
- img3={f3}
- img4={f4}
- img5={f5}
- img6={f6}
- img7={f7}
- img8={f8}
- img9={f9}
- 
- />
+<GaleriaProductos/>
+
 <div className="inicio-formulario" >
 <h4 >
             ¿Necesitas realizar un trabajo personalizado en hierro?
--- src/components/Secciones/SobreNosotros.jsx
+++ src/components/Secciones/SobreNosotros.jsx
@@ -1,42 +1,56 @@
-import React from 'react';
-import Portadas from '../Portadas/Portadas';
-import Footer from '../Footer/Footer';
-import imgsobre from '../imagenes/sobrenosotros.png';
+import React from "react";
+import Portadas from "../Portadas/Portadas";
+import Footer from "../Footer/Footer";
+import imgsobre from "../imagenes/sobrenosotros.png";
+import nosotros from "../imagenes/nosotros.png";
 import Whatsapp from "../Botones/Whatsapp";
 import "./Css/Sobrenosotros.css";
 
 function SobreNosotros() {
   return (
     <>
-    <Portadas
-    image={imgsobre}
-    texto='SOBRE NOSOTROS'
-    />
-    <Whatsapp/>
+      <Portadas image={imgsobre} texto="SOBRE NOSOTROS" />
+      <Whatsapp />
 
-<div className='container-nosotros'>
-<section className='container-nosotros-p'>
-  <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
-    Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-     The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+      <div className="container-nosotros">
+        <section className="container-nosotros-p">
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32. The standard chunk of
+            Lorem Ipsum used since the 1500s is reproduced below for those
+            interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
+            Malorum" by Cicero are also reproduced in their exact original form,
+            accompanied by English versions from the 1914 translation by H.
+            Rackham. It is a long established fact that a reader will be
+            distracted by the readable content of a page when looking at its
+            layout. The point of using Lorem Ipsum is that it has a more-or-less
+            normal distribution of letters, as opposed to using 'Content here,
+            content here', making it look like readable English. Many desktop
+            publishing packages and web page editors now use Lorem Ipsum as
+            their default model text, and a search for 'lorem ipsum' will
+            uncover many web sites still in their infancy. Various versions have
+            evolved over the years, sometimes by accident, sometimes on purpose
+            (injected humour and the like).
+          </p>
+        </section>
+        <div className="container-nosotros-img">
+          <img src={nosotros} alt="img-sobreNostros"></img>
+        </div>
+      </div>
 
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
- The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-  Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-   Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-</p>
-</section>
-<div className='container-nosotros-img'>
-  <img src='https://image.shutterstock.com/image-photo/engineer-worker-have-conversation-foundry-600w-702101359.jpg' alt='img-sobreNostros'></img>
-</div>
-</div>
-
-    <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default SobreNosotros
+export default SobreNosotros;
