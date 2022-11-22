bfa429ec5b3723b2e13395292320876fe6304426
AlvaroCordoba-5 <cordobaalvaro16@gmail.com>
2022-11-22 17:05:58 -0300

    final fixes

--- src/components/Botones/Boton.jsx
+++ src/components/Botones/Boton.jsx
@@ -5,7 +5,7 @@ function Boton({ texto }) {
   return (
     <>
       <a
-        href="http://api.whatsapp.com/send?phone=+5491123992937&text=Hola, te queria consultar por "
+        href="http://api.whatsapp.com/send?phone=+5491123992937&text=Hola, quiero cotizar mi proyecto! "
         target="_blank"
         style={{ textDecoration: "none" }}
       >
